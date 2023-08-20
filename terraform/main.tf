e0ff97d6de264cbc1847bcffaa5004fc5f94303a
C-D-Lewis <Bonsitm@gmail.com>
2023-08-20 11:22:56 +0100

    Add deployment pipeline

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_version = "= 1.2.9"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "= 4.31.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "chrislewis-tfstate"
+    key    = "xeno"
+    region = "us-east-1"
+  }
+}
+
+module "main" {
+  source = "github.com/c-d-lewis/terraform-s3-cloudfront-website?ref=master"
+
+  region          = "us-east-1"
+  project_name    = "xeno"
+  zone_id         = "Z05682866H59A0KFT8S"
+  domain_name     = "xeno.chrislewis.me.uk"
+  certificate_arn = "arn:aws:acm:us-east-1:617929423658:certificate/a69e6906-579e-431d-9e4c-707877d325b7"
+}
