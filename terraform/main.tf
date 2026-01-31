44be9ededa8675b2919a742316db142e0accb8a5
C-D-Lewis <Bonsitm@gmail.com>
2026-01-31 21:22:24 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {}
 
 terraform {
-  required_version = "= 1.2.9"
+  required_version = "~> 1.5.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "= 4.31.0"
+      version = "~> 4.31.0"
     }
   }
 
@@ -25,6 +25,5 @@ module "main" {
   zone_id             = "Z05682866H59A0KFT8S"
   domain_name         = "xeno.chrislewis.me.uk"
   certificate_arn     = "arn:aws:acm:us-east-1:617929423658:certificate/a69e6906-579e-431d-9e4c-707877d325b7"
-  logs_bucket         = "chrislewis-cloudwatch-logs"
   default_root_object = "dist/index.html"
 }
